7f694aa6954dd343eae3babc5fd1da2692a3474e
abeccari <47361308+abeccari@users.noreply.github.com>
2024-04-23 11:23:49 +0200

    Add straight wg cells and models

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
-import jax
+import inspect
+from collections.abc import Callable
+from functools import partial
+
 import jax.numpy as jnp
 import sax
 from gdsfactory.pdk import get_cross_section_name
 from gdsfactory.typings import CrossSectionSpec
 from numpy.typing import NDArray
-import lnoi400.tech
 
 from gplugins.sax.models import straight as __straight
 from gplugins.sax.models import bend as __bend
@@ -21,7 +23,6 @@ Float = float | FloatArray
 # Straights
 ################
 
-
 def _straight(
     *,
     wl: Float = 1.55,
@@ -37,18 +38,58 @@ def _straight(
                 loss = 2e-5,
                 wl0 = 1.55,
                 neff = 1.8,
-                ng = 2.22)    
+                ng = 2.22,
+        )
+
+    if cross_section_name == "xs_swg250":
+        return __straight(
+            wl = wl,
+            length = length,
+            loss = 1e-4,
+            wl0 = 1.55,
+            neff = 1.44,
+            ng = 1.7,
+        )
+
     else:
         raise ValueError(f"A model for the specified waveguide cross section {cross_section} is not defined.")
+    
+straight_rwg1000 = partial(_straight, cross_section="xs_rwg1000")
+straight_swg250 = partial(_straight, cross_section="xs_swg250")
+
     
 ################
 # Bends
 ################
 
+################
+# Models Dict
+################
+
+def get_models() -> dict[str, Callable[..., sax.SDict]]:
+    models = {}
+    for name, func in list(globals().items()):
+        if not callable(func):
+            continue
+        _func = func
+        while isinstance(_func, partial):
+            _func = _func.func
+        try:
+            sig = inspect.signature(_func)
+        except ValueError:
+            continue
+        if str(sig.return_annotation) == "sax.SDict":
+            models[name] = func
+    return models
+
 
 if __name__ == "__main__":
+    
+    import lnoi400.tech
+
     wl = jnp.linspace(1.5, 1.6, 1000)
-    S = _straight(wl = wl, length = 2e5)
+    model = straight_swg250
+    S = model(wl = wl, length = 2e3)
 
     plt.figure()
     plt.plot(wl, abs(S["o1", "o2"])**2)
